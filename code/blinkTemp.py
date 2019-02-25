039f66eee3f3f82bde5f80f5f2a2f428c4fc2555
sadia <khans24@hawkmail.newpaltz.edu>
2019-02-25 15:50:15 -0500

    update

--- code/blinkTemp.py
+++ code/blinkTemp.py
@@ -1,4 +1,4 @@
-#Import Libraries
+#Import Libraries we will be using
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
@@ -6,53 +6,59 @@ import os
 
 #Assign GPIO pins
 redPin = 27
-#greenPin = 22
+greenPin = 22
 tempPin = 17
 buttonPin = 26
 
 #Temp and Humidity Sensor
 tempSensor = Adafruit_DHT.DHT22
-#LED var
-#duration of blink
+#LED Variables-----------------------------------------------------------------------------------------
+#Duration of each Blink
 blinkDur = .1
-#number of time led blinks
+#Number of times to Blink the LED
 blinkTime = 7
-#---------------------
-#initialize GPIO
+#-----------------------------------------------------------------------------------------------------
+
+#Initialize the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(redPin,GPIO.OUT)
-#GPIO.setup(greenPin,GPIO.OUT)
+GPIO.setup(greenPin,GPIO.OUT)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def oneBlink(pin):
-    GPIO.output(pin,True)
-    time.sleep(blinkDur)
-    GPIO.output(pin,False)
-    time.sleep(blinkDur)
+	GPIO.output(pin,True)
+	time.sleep(blinkDur)
+	GPIO.output(pin,False)
+	time.sleep(blinkDur)
 
 def readF(tempPin):
-    humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPin)
-    temperature = temperature *9/5.0 +32
-    if humidity is None and temperaure is not None:
-        tempFahr = '{0:0.1f}*F'.format(temperature)
-    else:
-        print('Error Reading Sensor')
+	humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPin)
+	temperature = temperature * 9/5.0 +32
+	if humidity is not None and temperature is not None:
+		tempFahr = '{0:0.1f}*F'.format(temperature)
+	else:
+		print('Error Reading Sensor')
+	
+	return tempFahr
 
-    return tempFahr
+#def readH():
+#	humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPin)
+#	if humidity is not None and temperature is not None:
+#		humid = '{1:0.1f}%'.format(temperature, humidity)
+#
 
-#Use the blinkonce function in a loop when the button is pressed
+#Use the blinkonce function in a loopity-loop when the button is pressed
 try:
-    while True:
-        input_state = GPIO.input(buttonPin)
-        if input_state == False:
-            for i in range(blinkTime):
-                oneBlink(redPin)
-            time.sleep(.2)
-            data = readF(tempPin)
-            print (data)
-
+	while True:
+		input_state = GPIO.input(buttonPin)
+		if input_state == False:			
+			for i in range (blinkTime):
+				oneBlink(redPin)
+			time.sleep(.2)
+			data = readF(tempPin)
+			print (data)
+			
 except KeyboardInterrupt:
-    os.system('clear')
-    print('Thanks for Blinking!')
-    GPIO.cleanup()
-
+	os.system('clear')
+	print('Thanks for Blinking and Thinking!')
+	GPIO.cleanup()
